d3ad528f20fe503f7b35026e5e47361dd586232b
kazusakiii27 <184460707+kazusakiii27@users.noreply.github.com>
2026-03-29 12:43:45 +0700

    Add key inheritance in device.mk
    Add key inheritance for GSI boot verification.

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@
 
 DEVICE_PATH := device/xiaomi/garden
 
+# Keys
+$(call inherit-product, vendor/pr_signkey/keys/keys.mk)
+
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
